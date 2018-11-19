6847f7ef35027d17a0bb2913aefb9b0d2780a0ad
BT_1N3_03 <ljaoutakas8934@bths.edu>
2018-11-19 14:46:04 -0500

    lolmik0n9nmp

--- src/Runner.java
+++ src/Runner.java
@@ -4,7 +4,15 @@ public class Runner
     {
         StringChecker sc1 = new CodeWordChecker(5,8,"$");
         StringChecker sc2 = new CodeWordChecker("pass");
+        System.out.println(sc1.isVaild("happy"));
+        System.out.println(sc1.isVaild("happy$"));
+        System.out.println(sc1.isVaild("Code"));
+        System.out.println(sc1.isVaild("happyCode"));
 
+
+        System.out.println(sc2.isVaild("MyPass"));
+        System.out.println(sc2.isVaild("MypassPort"));
+        System.out.println(sc2.isVaild("happy"));
         System.out.println(sc2.isVaild("1,000,000,000,000,000"));
     }
 }
